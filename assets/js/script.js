25f25cbfab39f0567bed90cc33c4ae0dba4069d1
joeyo991 <joeyosull@gmail.com>
2021-12-10 15:14:53 +0000

    Create the question objects in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,86 @@
+let questionsAndAnswers = [
+    {
+        question: "Who is the all-time Premier League top scorer?",
+        a: "Wayne Rooney",
+        b: "Alan Shearer",
+        c: "Mohammed Salah",
+        d: "Harry Kane",
+        correctAnswer: "b",
+    },
+    {
+        question: "Which team has the most Premier League titles?",
+        a: "Chelsea",
+        b: "Arsenal",
+        c: "Manchester United",
+        d: "Manchester City",
+        correctAnswer: "c",
+    },
+    {
+        question: "Who has the most assists in Premier League history?",
+        a: "Ryan Giggs",
+        b: "Steven Gerrard",
+        c: "Thierry Henry",
+        d: "Trent Alexander-Arnold",
+        correctAnswer: "a",
+    },
+    {
+        question: "How many teams have won the Premier League?",
+        a: "5",
+        b: "6",
+        c: "7",
+        d: "8",
+        correctAnswer: "c",
+    },
+    {
+        question: "What is the record points total in a Premier League season?",
+        a: "100",
+        b: "114",
+        c: "106",
+        d: "98",
+        correctAnswer: "a",
+    },
+    {
+        question: "Who has the most Premier League appearances?",
+        a: "Mark Noble",
+        b: "Ryan Giggs",
+        c: "David James",
+        d: "Gareth Barry",
+        correctAnswer: "d",
+    },
+    {
+        question: "In what year did the Premier League begin?",
+        a: "1990",
+        b: "1995",
+        c: "1992",
+        d: "1996",
+        correctAnswer: "c",
+    },
+    {
+        question: "Who has played for the most Premier League clubs?",
+        a: "Nicolas Anelka",
+        b: "Craig Bellamy",
+        c: "Marcus Bent",
+        d: "Andy Cole",
+        correctAnswer: "c",
+    },
+    {
+        question: "Which player has not won the Premier League with two different clubs?",
+        a: "Frank Lampard",
+        b: "Robert Huth",
+        c: "Henning Berg",
+        d: "Ashley Cole",
+        correctAnswer: "a",
+    },
+    {
+        question: "Which of these teams have been relegated from the Premier league?",
+        a: "Everton",
+        b: "Tottenham Hotspur",
+        c: "Manchester City",
+        d: "Chelsea",
+        correctAnswer: "c",
+    }
+];
+
+let quiz = document.getElementById('quiz');
+let answers = document.getElementsByClassName('answer');
+
